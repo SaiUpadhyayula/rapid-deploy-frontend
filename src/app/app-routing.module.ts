5ece4d51f548dfff47414114f15fed1417098f77
saiupadhyayula <subramanyam.sai90@gmail.com>
2020-12-26 22:40:45 +0100

    commit frontend files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ApplicationComponent} from './application/application.component';
+import {CreateApplicationComponent} from './application/create-application/create-application.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'apps', component: ApplicationComponent},
+  {path: 'create-app', component: CreateApplicationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {ApplicationComponent} from './application/application.component';
+import {CreateApplicationComponent} from './application/create-application/create-application.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ApplicationDetailComponent } from './application/application-detail/application-detail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent,
+    FooterComponent,
+    ApplicationComponent,
+    CreateApplicationComponent,
+    ApplicationDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FontAwesomeModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
